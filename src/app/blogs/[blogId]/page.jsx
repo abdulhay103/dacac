5850f274042f738c5b8503b663c77656ba057c1c
abdulhay103 <abdulhay.me@gmail.com>
2024-02-07 11:32:42 +0600

    blog done

--- src/app/blogs/[blogId]/page.jsx
+++ src/app/blogs/[blogId]/page.jsx
@@ -1,14 +1,40 @@
 import BlogDetails from "@/components/blogs/BlogDetails";
 import SectionHeader from "@/components/utils/SectionHeader";
-import { getBlogDetails } from "@/utils/assetsData";
+import {
+    getAllBlogs,
+    getBlogDetails,
+    getDirectors,
+    getRelatedBlogs,
+    getServices,
+} from "@/utils/assetsData";
 
 export default async function BlogsDetails({ params }) {
     const id = parseInt(params.blogId);
-    let blogDetails = await getBlogDetails(id);
+    let blogDetailsPromise = getBlogDetails(id);
+    let latestBlogsPromose = getAllBlogs(4);
+    let servicesPromise = getServices(6);
+    let directorsPromise = getDirectors();
+
+    let [blogDetails, latestBlogs, services, directors] = await Promise.all([
+        blogDetailsPromise,
+        latestBlogsPromose,
+        servicesPromise,
+        directorsPromise,
+    ]);
+
+    let relatedBlogs = await getRelatedBlogs(blogDetails.categoryId);
     return (
         <>
             <SectionHeader pathName="blogs" title="Blog Details" />
-            <BlogDetails blogDetails={blogDetails} />
+            <BlogDetails
+                blogDatas={{
+                    blogDetails,
+                    latestBlogs,
+                    services,
+                    directors,
+                    relatedBlogs,
+                }}
+            />
         </>
     );
 }
--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -3,7 +3,7 @@ import SectionHeader from "@/components/utils/SectionHeader";
 import { getAllBlogs } from "@/utils/assetsData";
 
 export default async function Blogs() {
-    const blogs = await getAllBlogs();
+    const blogs = await getAllBlogs(6);
     return (
         <>
             <SectionHeader pathName="blogs" title="Our Portals" />
